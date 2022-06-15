1a8f51bd441d239e3b88f79be76971a82368b680
A1venG0 <artemlev11@ukr.net>
2022-06-15 21:37:54 +0300

    Added: Door in the spaceship

--- Assets/Scripts/Items/yellowCrystall.cs
+++ Assets/Scripts/Items/yellowCrystall.cs
@@ -19,16 +19,16 @@ public class YellowCrystall : Collidable
     /// <param name="coll">Collision</param>
     protected override void OnCollide(Collider2D coll)
     {
-        if (coll.name == "spaceman")
+        if (coll.name == "spaceman" && Inventory.instance.CheckItem("oxygenCrystall"))
         {
             iconToPickUp.SetActive(true);
-            if (Inventory.instance.CheckItem("oxygenCrystall") && Input.GetKeyDown("e"))
+            if (Input.GetKeyDown("e"))
             {
-                iconToPickUp.SetActive(false);
-                Instantiate(Resources.Load<GameObject>("Prefabs/yellowCristal"), new Vector3(-13.169f, -2.95f, 0), Quaternion.identity);
-                miniMap.SetActive(true);
-                //remove from inv
-                Inventory.instance.Remove(item);
+            iconToPickUp.SetActive(false);
+            Instantiate(Resources.Load<GameObject>("Prefabs/yellowCristal"), new Vector3(-13.169f, -2.95f, 0), Quaternion.identity);
+            miniMap.SetActive(true);
+            //remove from inv
+            Inventory.instance.Remove(item);
             }
         }
     }
